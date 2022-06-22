c177dc3014e855a5cd81931419c195b7c7c105fa
Spud97 <luca.dercole97@gmail.com>
2022-06-22 16:12:30 +0200

    Aggiunto header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheHeader
+},
+};
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;600;700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Asap Condensed", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-light py-0">
+      <div class="container d-flex justify-space-betwen">
+        <a class="navbar-brand" href="#">
+          <img src="/img/dc-logo.png" alt="" width="60" height="60" />
+        </a>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item" v-for="link in navLinks" :key="link">
+              <a class="nav-link active" aria-current="page" href="#">{{
+                link
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+  data() {
+    return {
+      navLinks: [
+        "CHARACTERS",
+        "COMICS",
+        "MOVIES",
+        "TV",
+        "GAMES",
+        "COLLECTIBLES",
+        "VIDEOS",
+        "FANS",
+        "NEWS",
+        "SHOP",
+      ],
+      //
+    };
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-link:hover {
+  color: rgb(70, 70, 255);
+  border-bottom: 3px solid rgb(70, 70, 255);
+}
+
+.nav-item {
+  font-size: 0.8rem;
+}
+
+</style>
